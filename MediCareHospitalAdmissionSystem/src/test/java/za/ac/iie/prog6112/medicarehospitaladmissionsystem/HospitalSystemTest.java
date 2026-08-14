e2108dd085c20642c90229a2dca0c3e28cc9cdfe
Avel-a <ST10506178@rcconnect.edu.za>
2026-08-14 11:45:48 +0200

    Creating .java classes

---
+++ MediCareHospitalAdmissionSystem/src/test/java/za/ac/iie/prog6112/medicarehospitaladmissionsystem/HospitalSystemTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package za.ac.iie.prog6112.medicarehospitaladmissionsystem;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author avela
+ */
+public class HospitalSystemTest {
+    
+    public HospitalSystemTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
